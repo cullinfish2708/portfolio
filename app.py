c98af2455bda8dec00e5efcbbedced095631f1b6
cullinfish2708 <ajisvengeance6699@gmail.com>
2026-04-15 00:22:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ elif page == "Resume":
     st.markdown("### 👀 Preview")
 
     st.markdown("""
-    <iframe src="https://drive.google.com/file/d/FILE_ID/preview" 
+    <iframe src="https://drive.google.com/file/d/1AL6rUctnZFNkDEqaxY32645LU8kAS26V/view?usp=drive_link" 
     width="100%" height="700px"></iframe>
     """, unsafe_allow_html=True)
 
